7569be14d397f635e30083245f1522dcd3df2b7d
tukka <aboodytukka@gmail.com>
2023-07-16 01:20:35 +0400

    ex04 done

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,22 +6,11 @@
 // #include <bits/stdc++>
 // using namespace std;
 
-void	ft_split(std::string	str, char *av[])
+void	ft_split(std::string str, char *av[], std::ofstream &file)
 {
-	std::istringstream s(str);
 	int					i;
 	std::string			w;
 
-	(void)av;
-	// while (s >> w)
-	// {
-	// 	if (w == av[2])
-	// 		std::cout << av[3];
-	// 	else
-	// 		std::cout << w + ' ';
-	// }
-	// 		std::cout << w + ' ';
-	// std::cout << str;
 	while (1)
 	{
 		i = str.find(' ');
@@ -29,41 +18,23 @@ void	ft_split(std::string	str, char *av[])
 			i += 1;
 		w = str.substr(0 , i);
 		if (w == av[2])
-			std::cout << av[3];
+			file << av[3];
 		else
-			std::cout << w;
+			file << w;
 		str = str.erase(0, i);
 		if (str.empty())
 			break;
-		// std::cout << "after trimming : " << str << std::endl;
-		// i = str.find(' ');
-		// if (i == 0)
-		// 	i += 1;
-		// w = str.substr(0 , i);
-		// if (w == av[2])
-		// 	std::cout << av[2];
-		// else
-		// 	std::cout << w;
-		// str = str.erase(i);
-		// if (str.empty())
-		// 	break ;
 	}
-	// str.substr(0)
-	// std::cout << "sub = " << str.substr(0 , str.find(' '));
-	// std::cout << w;
 }
 
 int main(int ac, char *av[])
 {
 	std::fstream	in;
+	std::string		out_name;
 	std::string		line;
-	std::string		tmp;
+	std::string		tmp = ".replace";
 	std::stringstream	buf;
-	// char		*h = new char[1000];
-	// char		*d = new char [1000];
-	// char			*t;
-	// int				j = 0;
-	// char			c;
+
 	if (ac == 4)
 	{
 		in.open(av[1], std::ios::in);
@@ -72,18 +43,16 @@ int main(int ac, char *av[])
 			std::cout << "File Error" << std::endl;
 			return (0);
 		}
+		out_name = av[1] + tmp;
+		std::ofstream	outfile(out_name);
 		while (std::getline(in, line))
 		{
-			// char *t = std::strtok(line , ' ');
-			// tmp += line;
-			ft_split(line, av);
-			std::cout << std::endl;
-			// std::strcpy(d, (char *)tmp);
-			// h = std::strtok(line, " ");
-			// d = std::strtok(h, " ");
+			ft_split(line, av, outfile);
+			if (!in.eof())
+				outfile << std::endl;
 		}
-		// std::cout << tmp << std::endl;
 		in.close();
+		outfile.close();
 	}
 	else
 	{
