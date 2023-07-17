6dc7bbd52894543b04dc4b28f50588d9ff40da3e
A Tukka <atukka@Tukkas-MacBook.local>
2023-07-17 20:04:46 +0400

    ex06 d

--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -4,8 +4,6 @@ int main(int ac, char *av[])
 {
     Harl    harl;
 
-    // void (Harl::*ptr)(std::string) = &Harl ::complain;
-    // (harl.*ptr)("error");
     if (ac == 2)
     {
         harl.complain(av[1]);
