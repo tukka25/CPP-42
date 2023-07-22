abd022325e8dd37794f8985c9ccd8bff5dc6c0c2
tukka25 <aboodytukka@gmail.com>
2023-07-23 01:16:04 +0400

    cpp02/ex00 done

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -2,6 +2,7 @@
 
 Fixed::Fixed()
 {
+	std::cout << "Default Constructor Called" << std::endl;
 	num = 0;
 	// frac = 8;
 }
@@ -16,11 +17,13 @@ Fixed::Fixed(const Fixed &t)
 
 int Fixed::getRawBits( void ) const
 {
+	std::cout << "getRawBits function called" << std::endl;
 	return (num);
 }
 
 void	Fixed::operator=(const Fixed &t)
 {
+	std::cout << "Copy Assignment Operator Called" << std::endl;
 	num = t.num;
 }
 
@@ -31,4 +34,5 @@ void Fixed::setRawBits( int const raw )
 
 Fixed::~Fixed()
 {
+	std::cout << "Destructor Called" << std::endl;
 }
