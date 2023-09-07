86b9fead1c41b138fbf8c1230f0fcb1762fce80e
tukka25 <aboodytukka@gmail.com>
2023-09-07 12:31:58 +0400

    cpp08/ex01 span is done

--- cpp08/ex01/Span.cpp
+++ cpp08/ex01/Span.cpp
@@ -17,20 +17,29 @@ Span::Span(unsigned int n)
 void    Span::addNumber(int a)
 {
     static int i;
-    std::multiset<int>::iterator  t = this->arr.begin();
+    // int         k = 0;
+    // std::multiset<int>::iterator  t = this->arr.begin();
 
-    // std::cout << "t = " << *t << std::endl;
+    // std::cout << "a = " << a << std::endl;
     if (i == max)
         throw (fullySet());
     this->arr.insert(a);
     if (i >= 1)
     {
-        std::cout << "w = " << *this->arr.begin() << std::endl;
-        for (int j = 0; j < i; j++)
-            t++;
-        this->shortest = *--t - *t;
+        // std::cout << "w = " << *this->arr.begin() << std::endl;
+        // for (int j = 0; j < i; j++)
+        // {
+        //     t++;
+        //     k++;
+        // }
+        // k = (a - *--this->arr.end());
+        // if (k < 0)
+        //     k *= -1;
+        // std::cout << "first = " << *----this->arr.end() << std::endl;
+        // std::cout << "second = " << *--this->arr.end() << std::endl;
+        // if (k < this->shortest)
+        //     this->shortest = k;
     }
-    std::cout << "this->" << this->shortest << std::endl;
     i++;
 }
 
@@ -45,9 +54,19 @@ void    Span::printt(void)
 
 int Span::shortestSpan(void)
 {
+    int tmp = 0;
+
     if (this->arr.size() <= 1)
         throw (noSpan());
-    return (*this->arr.begin() - *++this->arr.begin());
+    std::multiset<int>::iterator t = ++this->arr.begin();
+    for (;t != this->arr.end(); t++)
+    {
+        tmp = *--t;
+        ++t;
+        if (tmp - *t < this->shortest)
+            this->shortest = tmp - *t;
+    }
+    return (this->shortest);
 }
 
 int Span::longestSpan(void)
--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -7,9 +7,9 @@ int main()
         Span sp = Span(5);
         sp.addNumber(6);
         sp.addNumber(3);
-        // sp.addNumber(17);
-        // sp.addNumber(9);
-        // sp.addNumber(11);
+        sp.addNumber(17);
+        sp.addNumber(9);
+        sp.addNumber(11);
         std::cout << sp.shortestSpan() << std::endl;
         std::cout << sp.longestSpan() << std::endl;
     }
