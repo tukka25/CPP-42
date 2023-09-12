3814d4611f263e075e7863c41f26e596ecaa38d3
tukka25 <aboodytukka@gmail.com>
2023-09-12 22:31:23 +0400

    first one is done

--- cpp09/ex01/RPN.cpp
+++ cpp09/ex01/RPN.cpp
@@ -7,46 +7,34 @@ Rpn::Rpn()
 
 void    Rpn::execution(char *argv)
 {
-    // int             tmp;
     std::string s;
     int          g;
     int          g2;
     std::stringstream   ss;
 
-    // s = argv[1];
     s = argv;
-    // std::cout << "s out = " << s << std::endl;
     if (s.find_first_not_of("0987654321 *+-/") != std::string::npos || s.empty())
         throw (WrongInput());
     ss << argv;
     while (getline(ss, s, ' '))
     {
-        std::cout << "s = " << s << std::endl;
         if (s.size() > 1)
             throw (WrongInput());
         if (s.find_first_not_of("0987654321") == std::string::npos)
         st.push(std::atoi(s.c_str()));
-        // if (st.size() > 2 && s.find_first_not_of("+-/*") != std::string::npos)
-        //     throw (WrongInput());
         if (st.size() >= 2 && s.find_first_of("+-/*") != std::string::npos)
         {
             g = st.top();
             st.pop();
             g2 = st.top();
             st.pop();
-            std::cout << "gn = " << g << " g2 n = " << g2 << "s = " << s << std::endl; 
-            // std::cout << "g = " << this->doOperation(g2, g, s) << std::endl;
             st.push(this->doOperation(g2, g, s));
             std::cout << st.top() << std::endl;
-            // break ;
         } 
     }
-    // std::cout << st.size() << std::endl;
-    while (!st.empty())
-    {
-        std::cout << "in stack = "  << st.top() << std::endl;
-        st.pop();
-    }
+    if (st.size() >= 2)
+        throw (WrongInput());
+    std::cout << "Result => "  << st.top() << std::endl;
     
 }
 
