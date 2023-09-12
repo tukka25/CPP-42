9ef1c567cb1fdaa016b17fd2fb02b989fefd4b90
tukka25 <aboodytukka@gmail.com>
2023-09-12 21:00:38 +0400

    cpp09/ex01 done but only small parsing errors

--- cpp09/ex01/RPN.cpp
+++ cpp09/ex01/RPN.cpp
@@ -22,23 +22,29 @@ void    Rpn::execution(char *argv)
     while (getline(ss, s, ' '))
     {
         std::cout << "s = " << s << std::endl;
-        st.push(std::atoi(s.c_str()));
-        if (st.size() > 2 && s.find_first_not_of("+-/*") != std::string::npos)
+        if (s.size() > 1)
             throw (WrongInput());
-        if (st.size() == 2)
+        if (s.find_first_not_of("0987654321") == std::string::npos)
+        st.push(std::atoi(s.c_str()));
+        // if (st.size() > 2 && s.find_first_not_of("+-/*") != std::string::npos)
+        //     throw (WrongInput());
+        if (st.size() >= 2 && s.find_first_of("+-/*") != std::string::npos)
         {
             g = st.top();
             st.pop();
             g2 = st.top();
             st.pop();
-            std::cout << "g = " << this->doOperation(g, g2, s) << std::endl;
-            st.push(this->doOperation(g, g2, s));
+            std::cout << "gn = " << g << " g2 n = " << g2 << "s = " << s << std::endl; 
+            // std::cout << "g = " << this->doOperation(g2, g, s) << std::endl;
+            st.push(this->doOperation(g2, g, s));
+            std::cout << st.top() << std::endl;
             // break ;
         } 
     }
+    // std::cout << st.size() << std::endl;
     while (!st.empty())
     {
-        std::cout << st.top() << std::endl;
+        std::cout << "in stack = "  << st.top() << std::endl;
         st.pop();
     }
     
